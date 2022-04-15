42d9538ff0c98bc2403557bb29fe1fb4e63bc122
RNPC <schollmeyer.dan@gmail.com>
2022-04-15 12:02:35 -0500

    added ingredient

---
+++ Schollmeyer_Dan_ClayBrewing_CaseStudy/src/main/java/com/optum/Ingredient/Ingredient.java
@@ -0,0 +1,84 @@
+package com.example.claybrewing;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private String description;
+    private String quantityType;
+    private int quantity;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(int id, String name, String description, String quantityType) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantityType = quantityType;
+        this.quantity = 0;
+        save();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantityType() {
+        return quantityType;
+    }
+
+    public void setQuantityType(String quantityType) {
+        this.quantityType = quantityType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean create(int id, String name, String description, String quantityType) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantityType = quantityType;
+        this.quantity = 0;
+        return save();
+    }
+
+    public boolean update(String name, String description, String quantityType, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.quantityType = quantityType;
+        this.quantity = quantity;
+        return save();
+    }
+
+    private boolean save() {
+        Inventory.saveIngredient(this);
+        return Inventory.getInventoryMap().containsKey(this.id);
+        //update method for failure? connect to db.
+    }
+}
